7ce2c7b56861f66ba4a61d2495a038374ed854dc
whfxiaoe <1434005779@qq.com>
2019-03-15 13:38:13 +0800

    byOnline

--- src/test/java/com/example/buyonline/BuyonlineApplicationTests.java
+++ src/test/java/com/example/buyonline/BuyonlineApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.pms;
+package com.example.buyonline;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PmsApplicationTests {
+public class BuyonlineApplicationTests {
 
     @Test
     public void contextLoads() {
